c4e105240cce6ad7a512ce0db2efd6a7de15e0d5
gautamkumar30 <gautamkumarganesan@gmail.com>
2024-07-24 19:42:46 +0530

    add new component

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -1,5 +1,9 @@
 import "./Dashboard.scss";
 
+const DisplayCard = () => {
+  return <p>DisplayCard</p>;
+};
+
 const DashboardCard = ({ value, title1, title2 }) => {
   return (
     <div className="dashboardCard">

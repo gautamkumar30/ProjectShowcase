38c26917dddeb9d2989e6060506c2ef51989858c
gautamkumar30 <137641850+gautamkumar30@users.noreply.github.com>
2024-07-27 23:54:36 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,7 +7,7 @@ export const AuthContext = createContext();
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({});
 
-  useEffect(() => {
+  useEffect(() => { 
     const unsub = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
     });
